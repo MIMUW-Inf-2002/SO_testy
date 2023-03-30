48bf83ae37eb8ae64d2cbade554e5b28408b3500
Maksym Ratajczyk <m.ratajczyk6@student.uw.edu.pl>
2023-03-30 17:00:04 +0200

    Dodany test typu time benchmark.

--- zad1/main.c
+++ zad1/main.c
@@ -1,5 +1,6 @@
 #include "rentib.h"
 #include "kwasow.h"
+#include "mrarat.h"
 
 #include <stdio.h>
 
@@ -7,6 +8,9 @@ int main(void) {
   printf("\n=> Running Kwasow tests\n");
   kwasow_test();
   
+  printf("\n=> Running Mrarat tests\n");
+  mrarat_test();
+
   printf("=> Running Rentib tests\n");
   unsigned int rentib_seed = 213742069;
   rentib_test(rentib_seed);
