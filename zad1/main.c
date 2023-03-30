644bc2edd29a53b3b9af34401148be9002b7e96b
Igor Kamiński <52384567+rog1gor@users.noreply.github.com>
2023-03-30 12:41:52 +0200

    Updating main to run rog1gor's tests

--- zad1/main.c
+++ zad1/main.c
@@ -1,5 +1,6 @@
 #include "rentib.h"
 #include "kwasow.h"
+#include "rog1gor.h"
 
 #include <stdio.h>
 
@@ -10,4 +11,8 @@ int main(void) {
   printf("=> Running Rentib tests\n");
   unsigned int rentib_seed = 213742069;
   rentib_test(rentib_seed);
+
+  unsigned int rog1gor_seed = 146543;
+  printf("=> Running rog1gor tests\n");
+  rog1gor_test(rog1gor_seed);
 }
