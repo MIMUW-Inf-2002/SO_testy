f9a12f8d927b8bd78527c780238b272ec9673d3c
Karol Wąsowski <wasowski02@protonmail.com>
2023-04-12 11:13:05 +0200

    Added core_test function

---
+++ zad2/core_test.asm
@@ -0,0 +1,62 @@
+section .data
+test_string db '245+*', 0
+
+section .rodata
+error_msg db 'core_test failed - registers not preserved', 0
+error_msg_len equ 38
+
+section .text
+global core_test
+extern core
+
+%macro fail_test 0
+  ; Print error
+  mov eax, 1                    ; Set syscall number for write()
+  mov edi, 1                    ; Set file descriptor for stdout
+  lea rsi, [rel error_msg]      ; Set address of error message string
+  mov rdx, error_msg_len        ; Set length of error message string
+  syscall                       ; Call write()
+  ; Exit program
+  mov eax, 1
+  xor ebx, ebx
+  inc ebx                       ; Set error code to 1
+  int 0x80                      ; Exit program
+%endmacro
+
+; uint64_t core_test(uint64_t n, char const *p);
+; Same interface as core
+core_test:
+  ; Save on stack
+  push  rbp
+  push  rbx
+  push  r12
+  push  r13
+  push  r14
+  push  r15
+  sub   rsp, 0x8
+  ; Call
+  ; rdi and rsi are already set
+  call  core
+  add   rsp, 0x8
+  ; Test
+  cmp   r15, [rsp]
+  jne   .error
+  pop   r15
+  cmp   r14, [rsp]
+  jne   .error
+  pop   r14
+  cmp   r13, [rsp]
+  jne   .error
+  pop   r13
+  cmp   r12, [rsp]
+  jne   .error
+  pop   r12
+  cmp   rbx, [rsp]
+  jne   .error
+  pop   rbx
+  cmp   rbp, [rsp]
+  jne   .error
+  pop   rbp
+  ret
+.error:
+  fail_test
--- zad2/kwasow_simple.c
+++ zad2/kwasow_simple.c
@@ -9,7 +9,7 @@
 #define N 2
 #endif
 
-uint64_t core(uint64_t n, char const *p);
+uint64_t core_test(uint64_t n, char const *p);
 uint64_t get_value(uint64_t n) {
   assert(n < N);
   return n + 1;
@@ -22,54 +22,56 @@ void put_value(uint64_t n, uint64_t v) {
 /* ================================== TEST ================================== */
 int main(void) {
   // Basic, single operation tests
-  assert(core(0, "9") == 9);
-  assert(core(0, "934") == 4);
+  assert(core_test(0, "9") == 9);
+  assert(core_test(0, "934") == 4);
   printf("[0-9] passed\n");
 
-  assert(core(0, "12+") == 3);
+  assert(core_test(0, "12+") == 3);
   printf("+     passed\n");
 
-  assert(core(0, "12*") == 2);
-  assert(core(0, "22*") == 4);
-  assert(core(0, "34*") == 12);
-  assert(core(0, "4827*") == 14);
-  assert(core(0, "48*27**") == 448);
+  assert(core_test(0, "12*") == 2);
+  assert(core_test(0, "22*") == 4);
+  assert(core_test(0, "34*") == 12);
+  assert(core_test(0, "4827*") == 14);
+  assert(core_test(0, "48*27**") == 448);
   printf("*     passed\n");
 
-  assert(core(0, "0-") == 0);
-  assert(core(0, "1-") == UINT64_MAX);
-  assert(core(0, "3-") == UINT64_MAX - 2);
-  assert(core(0, "19-") == UINT64_MAX - 8);
-  assert(core(0, "19-8+") == UINT64_MAX);
-  assert(core(0, "19-9+") == 0);
+  assert(core_test(0, "0-") == 0);
+  assert(core_test(0, "1-") == UINT64_MAX);
+  assert(core_test(0, "3-") == UINT64_MAX - 2);
+  assert(core_test(0, "19-") == UINT64_MAX - 8);
+  assert(core_test(0, "19-8+") == UINT64_MAX);
+  assert(core_test(0, "19-9+") == 0);
   printf("-     passed\n");
 
-  assert(core(0, "n") == 0);
-  assert(core(12, "n") == 12);
-  assert(core(UINT64_MAX, "n") == UINT64_MAX);
+  assert(core_test(0, "n") == 0);
+  assert(core_test(12, "n") == 12);
+  assert(core_test(UINT64_MAX, "n") == UINT64_MAX);
   printf("n     passed\n");
 
-  assert(core(0, "12C") == 1);
-  assert(core(0, "1243C") == 4);
-  assert(core(0, "123CCC9") == 9);
+  assert(core_test(0, "12C") == 1);
+  assert(core_test(0, "1243C") == 4);
+  assert(core_test(0, "123CCC9") == 9);
   printf("C     passed\n");
 
-  assert(core(0, "1DC") == 1);
-  assert(core(0, "1D2") == 2);
-  assert(core(0, "1D4D") == 4);
-  assert(core(0, "1298DC") == 8);
-  assert(core(0, "98DCDCDCDC") == 8);
-  assert(core(0, "98DCDCDCDCC") == 9);
+  assert(core_test(0, "1DC") == 1);
+  assert(core_test(0, "1D2") == 2);
+  assert(core_test(0, "1D4D") == 4);
+  assert(core_test(0, "1298DC") == 8);
+  assert(core_test(0, "98DCDCDCDC") == 8);
+  assert(core_test(0, "98DCDCDCDCC") == 9);
   printf("D     passed\n");
 
-  assert(core(0, "12E") == 1);
-  assert(core(0, "1243E") == 4);
-  assert(core(0, "1243EC") == 3);
-  assert(core(0, "1243ECC") == 2);
-  assert(core(0, "1243ECCC") == 1);
+  assert(core_test(0, "12E") == 1);
+  assert(core_test(0, "1243E") == 4);
+  assert(core_test(0, "1243EC") == 3);
+  assert(core_test(0, "1243ECC") == 2);
+  assert(core_test(0, "1243ECCC") == 1);
   printf("E     passed\n");
 
-  assert(core(0, "G") == 1);
-  assert(core(1, "G") == 2);
+  assert(core_test(0, "G") == 1);
+  assert(core_test(1, "G") == 2);
   printf("G     passed\n");
+
+  // printf("B     passed\n");
 }
